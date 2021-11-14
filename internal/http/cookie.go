09a521ae6328f87868a1faaa691e177ff413cffa
James <tjamesperry@hotmail.com>
2021-11-13 18:43:27 -0600

    http: created SetSessionCookie and SessionCookieFromRequest to interact with cookies in repeatable manner

---
+++ internal/http/cookie.go
@@ -0,0 +1,36 @@
+package http
+
+import "net/http"
+
+type CookieOptions struct {
+	Domain   string
+	Secure   bool
+	SameSite http.SameSite
+}
+
+func SetSessionCookie(
+	w http.ResponseWriter,
+	id string,
+	options CookieOptions,
+) {
+	http.SetCookie(
+		w,
+		&http.Cookie{
+			Name:     sessionKey,
+			Value:    id,
+			Domain:   options.Domain,
+			Path:     "/",
+			Secure:   options.Secure,
+			HttpOnly: true,
+			SameSite: options.SameSite,
+		},
+	)
+}
+
+func SessionFromRequest(req *http.Request) string {
+	cookie, err := req.Cookie(sessionKey)
+	if err != nil {
+		return ""
+	}
+	return cookie.Value
+}
