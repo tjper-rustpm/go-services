0876610ffbc4780e4db01d9d6cd745072b9aebe3
James <tjamesperry@hotmail.com>
2021-12-01 00:20:15 -0600

    internal/http: updated Session middleware to touch session and evaluate absolute expiration

--- internal/http/session.go
+++ internal/http/session.go
@@ -4,17 +4,21 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/tjper/rustcron/internal/session"
 	"go.uber.org/zap"
 )
 
-// Retriever retrieves a Session for the current process.
-type Retriever interface {
+// ISessionManager encompasses all manners by which a session may be interacted
+// with.
+type ISessionManager interface {
 	Retrieve(context.Context, string) (*session.Session, error)
+	Touch(context.Context, string, time.Duration) error
+	Delete(context.Context, string) error
 }
 
-func SessionAuthenticated(logger *zap.Logger, retriever Retriever) func(http.Handler) http.Handler {
+func Session(logger *zap.Logger, manager ISessionManager, exp time.Duration) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
@@ -24,12 +28,31 @@ func SessionAuthenticated(logger *zap.Logger, retriever Retriever) func(http.Han
 					return
 				}
 
-				sess, err := retriever.Retrieve(r.Context(), sessionID)
+				sess, err := manager.Retrieve(r.Context(), sessionID)
 				if errors.Is(err, session.ErrSessionDNE) {
 					ErrUnauthorized(w)
 					return
 				}
 				if err != nil {
+					ErrInternal(logger, w, err)
+					return
+				}
+
+				if sess.AbsoluteExpiration.Before(time.Now()) {
+					logger.Info(
+						"deleting base on absolute expiration",
+						zap.Time("exp", sess.AbsoluteExpiration),
+						zap.Time("now", time.Now()),
+					)
+					if err := manager.Delete(r.Context(), sessionID); err != nil {
+						logger.Error("error deleting session", zap.Error(err))
+					}
+
+					ErrUnauthorized(w)
+					return
+				}
+
+				if err := manager.Touch(r.Context(), sessionID, exp); err != nil {
 					ErrInternal(logger, w, err)
 					return
 				}
