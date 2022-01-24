02371f80a8016b5364cfbe348d6a714fe029d6c7
James <tjamesperry@hotmail.com>
2022-01-23 19:35:53 -0600

    internal/email: updated Mock type to allow for checking sent emails

--- internal/email/mock.go
+++ internal/email/mock.go
@@ -9,14 +9,14 @@ func NewMock() *Mock {
 	return &Mock{
 		mutex:               new(sync.RWMutex),
 		passwordResetEmails: make(map[string]string),
-		sendVerifyEmails:    make(map[string]string),
+		verifyEmails:        make(map[string]string),
 	}
 }
 
 type Mock struct {
 	mutex               *sync.RWMutex
 	passwordResetEmails map[string]string
-	sendVerifyEmails    map[string]string
+	verifyEmails        map[string]string
 }
 
 func (m *Mock) SendPasswordReset(ctx context.Context, to, hash string) error {
@@ -27,10 +27,22 @@ func (m *Mock) SendPasswordReset(ctx context.Context, to, hash string) error {
 	return nil
 }
 
-func (m Mock) SendVerifyEmail(ctx context.Context, to, hash string) error {
+func (m *Mock) PasswordResetHash(to string) string {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.passwordResetEmails[to]
+}
+
+func (m *Mock) SendVerifyEmail(ctx context.Context, to, hash string) error {
 	m.mutex.Lock()
 	defer m.mutex.Unlock()
 
-	m.sendVerifyEmails[to] = hash
+	m.verifyEmails[to] = hash
 	return nil
+}
+
+func (m *Mock) VerifyEmailHash(to string) string {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+	return m.verifyEmails[to]
 }
