ee39a801d040edef3f30080ba58d2476c036e613
James <tjamesperry@hotmail.com>
2022-01-25 21:30:26 -0600

    cmd/payment: added db initialization and migration to service launch

--- cmd/payment/config/config.go
+++ cmd/payment/config/config.go
@@ -3,8 +3,13 @@ package config
 import "github.com/spf13/viper"
 
 const (
-	envPrefix    = "PAYMENT"
-	keyStripeKey = "STRIPE_KEY"
+	envPrefix        = "PAYMENT"
+	keyPort          = "PORT"
+	keyDSN           = "DSN"
+	keyMigrations    = "MIGRATIONS"
+	keyStripeKey     = "STRIPE_KEY"
+	keyRedisAddr     = "REDIS_ADDR"
+	keyRedisPassword = "REDIS_PASSWORD"
 )
 
 func Load() *Config {
@@ -21,9 +26,17 @@ type Config struct {
 }
 
 func (c *Config) defaults() {
+	c.v.SetDefault(keyPort, 8080)
+	c.v.SetDefault(keyDSN, "host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable TimeZone=UTC")
+	c.v.SetDefault(keyMigrations, "file:///db/migrations")
 	c.v.SetDefault(keyStripeKey, "")
+	c.v.SetDefault(keyRedisAddr, "redis:6379")
+	c.v.SetDefault(keyRedisPassword, "")
 }
 
-func (c Config) StripeKey() string {
-	return c.v.GetString(keyStripeKey)
-}
+func (c Config) DSN() string           { return c.v.GetString(keyDSN) }
+func (c Config) Migrations() string    { return c.v.GetString(keyMigrations) }
+func (c Config) Port() int             { return c.v.GetInt(keyPort) }
+func (c Config) StripeKey() string     { return c.v.GetString(keyStripeKey) }
+func (c Config) RedisAddr() string     { return c.v.GetString(keyRedisAddr) }
+func (c Config) RedisPassword() string { return c.v.GetString(keyRedisPassword) }
--- cmd/payment/main.go
+++ cmd/payment/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/tjper/rustcron/cmd/payment/config"
 	"github.com/tjper/rustcron/cmd/payment/controller"
+	"github.com/tjper/rustcron/cmd/payment/db"
+	"github.com/tjper/rustcron/cmd/payment/rest"
+	ihttp "github.com/tjper/rustcron/internal/http"
+	"github.com/tjper/rustcron/internal/session"
 
+	redisv8 "github.com/go-redis/redis/v8"
 	"github.com/stripe/stripe-go/v72/client"
 	"go.uber.org/zap"
 )
@@ -20,9 +29,11 @@ const (
 	_
 	ecDatabaseConnection
 	ecMigration
+	ecRedisConnection
 )
 
 func run() int {
+	ctx := context.Background()
 	cfg := config.Load()
 
 	logger, err := zap.NewDevelopment()
@@ -31,14 +42,69 @@ func run() int {
 	}
 	defer func() { _ = logger.Sync() }()
 
+	logger.Info("[Startup] Connecting to DB ...")
+	dbconn, err := db.Open(cfg.DSN())
+	if err != nil {
+		logger.Error(
+			"[Startup] Failed to initialize database connection.",
+			zap.Error(err),
+		)
+		return ecDatabaseConnection
+	}
+	logger.Info("[Startup] Connected to DB.")
+
+	logger.Info("[Startup] Migrating DB ...")
+	if err := db.Migrate(dbconn, cfg.Migrations()); err != nil {
+		logger.Error(
+			"[Startup] Failed to migrate database model.",
+			zap.Error(err),
+		)
+		return ecMigration
+	}
+	logger.Info("[Startup] Migrated DB.")
+
+	logger.Info("[Startup] Connecting to Redis ...")
+	rdb := redisv8.NewClient(&redisv8.Options{
+		Addr:     cfg.RedisAddr(),
+		Password: cfg.RedisPassword(),
+	})
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		logger.Error(
+			"[Startup] Failed to initialize Redis client.",
+			zap.Error(err),
+		)
+		return ecRedisConnection
+	}
+	logger.Info("[Startup] Connected to Redis.")
+
 	stripe := &client.API{}
 	stripe.Init(cfg.StripeKey(), nil)
 
+	logger.Info("[Startup] Creating session manager ...")
+	sessionManager := session.NewManager(logger, rdb)
+	logger.Info("[Startup] Created session manager.")
+
+	logger.Info("[Startup] Creating controller ...")
 	ctrl := controller.New(
 		logger,
 		stripe.CheckoutSessions,
 		stripe.BillingPortalSessions,
 	)
+	logger.Info("[Startup] Created controller.")
 
+	logger.Info("[Startup] Creating REST API ...")
+	api := rest.NewAPI(
+		logger,
+		ctrl,
+		ihttp.NewSessionMiddleware(
+			logger,
+			sessionManager,
+			48*time.Hour, // 2 days
+		),
+	)
+	logger.Info("[Startup] Created REST API.")
+
+	logger.Sugar().Infof("[Startup] payment API listening at :%d", cfg.Port())
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port()), api.Mux))
 	return ecExit
 }
