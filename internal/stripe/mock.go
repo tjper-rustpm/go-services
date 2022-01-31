2a5dcc580cfd868818ea5e9152e1b6fcac807120
James <tjamesperry@hotmail.com>
2022-01-30 21:06:37 -0600

    internal/stripe: created Mock type to mock stripe usage

---
+++ internal/stripe/mock.go
@@ -0,0 +1,36 @@
+package stripe
+
+import "github.com/stripe/stripe-go/v72"
+
+func NewMock() *Mock {
+	return &Mock{
+		checkoutSessions: make([]*stripe.CheckoutSessionParams, 0),
+	}
+}
+
+type Mock struct {
+	checkoutSessions      []*stripe.CheckoutSessionParams
+	billingPortalSessions []*stripe.BillingPortalSessionParams
+}
+
+func (m *Mock) CheckoutSession(params *stripe.CheckoutSessionParams) (string, error) {
+	m.checkoutSessions = append(m.checkoutSessions, params)
+	return "", nil
+}
+
+func (m *Mock) PopCheckoutSession() *stripe.CheckoutSessionParams {
+	session := m.checkoutSessions[0]
+	m.checkoutSessions = m.checkoutSessions[1:]
+	return session
+}
+
+func (m *Mock) BillingPortalSession(params *stripe.BillingPortalSessionParams) (string, error) {
+	m.billingPortalSessions = append(m.billingPortalSessions, params)
+	return "", nil
+}
+
+func (m *Mock) PopBillingPortalSession() *stripe.BillingPortalSessionParams {
+	session := m.billingPortalSessions[0]
+	m.billingPortalSessions = m.billingPortalSessions[1:]
+	return session
+}
